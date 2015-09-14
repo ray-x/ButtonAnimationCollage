847ab94b7678b352a87815638b77481d9619d990
Ray-x <rayx.cn@gmail.com>
2015-09-14 13:58:02 +1000

    add comments and readme

---
+++ ButtonAnimationCollage/UIButton+Effects.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+Effects.h
+//  ButtonAnimationCollage
+//
+//  Created by Ray Xu on 23/08/2015.
+//  Copyright (c) 2015 Ray Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (Effects)
+
+@end
---
+++ ButtonAnimationCollage/UIButton+Effects.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+Effects.m
+//  ButtonAnimationCollage
+//
+//  Created by Ray Xu on 23/08/2015.
+//  Copyright (c) 2015 Ray Xu. All rights reserved.
+//
+
+#import "UIButton+Effects.h"
+
+@implementation UIButton (Effects)
+
+@end
